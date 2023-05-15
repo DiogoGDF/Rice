fde5116590cf48f49e78478f799f1f3d6c6a7639
DiogoGDF <diogogarbinato@gmail.com>
2023-05-15 01:06:23 -0300

    Simple changes

---
+++ scripts/LeftwmGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf ~/Documents/Rice/leftwm/themes
+cp -r ~/.config/leftwm/themes ~/Documents/Rice/leftwm
+rm ~/Documents/Rice/leftwm/config.ron
+cp ~/.config/leftwm/config.ron ~/Documents/Rice/leftwm
